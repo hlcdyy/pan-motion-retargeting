51e505cb53bd3c2fba6e1dc098eeadc95316a380
Li Jie <cpunion@gmail.com>
2024-03-11 09:40:01 +0800

    Add pretrained and datasets download scripts

---
+++ download_pretrained.sh
@@ -0,0 +1,7 @@
+gdown -O pretrained_mixamo.zip 1jYtOLCDye68nShXNlse-I5hCe7ZAHaG-
+unzip pretrained_mixamo.zip
+rm pretrained_mixamo.zip
+
+gdown -O pretrained_lafan1dog.zip 1p-fDC9nIuqktVaqxcAr4wSa09mGq1_63
+unzip pretrained_lafan1dog.zip
+rm pretrained_lafan1dog.zip
